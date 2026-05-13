51330ce288c098aacb81c739fa271e386d58248e
juan jose <herrerajuanjose825@gmail.com>
2026-05-13 09:23:21 +0200

    HU5
    Signed-off-by: juan jose <herrerajuanjose825@gmail.com>

---
+++ cliente.java
@@ -0,0 +1,59 @@
+public class cliente {
+    private String cedula;
+    private String nombre;
+    private String apellido;
+    private String telefono;
+    private String direccion;
+    private String licencia;
+
+    public cliente() {
+    }
+    
+    public cliente(String cedula, String nombre, String apellido, String telefono, String direccion, String licencia) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.direccion = direccion;
+        this.licencia = licencia;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getApellido() {
+        return apellido;
+    }
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    public String getTelefono() {
+        return telefono;
+    }
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    public String getDireccion() {
+        return direccion;
+    }
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    public String getLicencia() {
+        return licencia;
+    }
+    public void setLicencia(String licencia) {
+        this.licencia = licencia;
+    }
+
+    
+}
